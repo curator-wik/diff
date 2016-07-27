9bdfd74d1a729a6d76cc2f6bd940d504dda6b705
Juan Gonzalez <supergusano2@gmail.com>
2016-07-26 21:36:16 -0500

    very simple program that reads the git repo in current directory and prints the commit messages

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <git2.h>
 
+#define exit_on_error(x) _exit_on_error(x, __FUNCTION__, __LINE__)
+
+void _exit_on_error(int error, const char *func, const int line)
+{
+	if(!error)
+		return;
+	const git_error *e = giterr_last();
+	printf("%s-%d - Error %d/%d: %s\n", func, line, error, e->klass, e->message);
+	exit(error);
+}
+
 int main(int argc, char **argv)
 {
 	git_libgit2_init();
-	
+	git_oid oid;
 	git_repository *repo = NULL;
+	git_commit *commit = NULL;
 	int error = git_repository_open(&repo, ".");
-	if (error < 0) {
-		const git_error *e = giterr_last();
-		printf("Error %d/%d: %s\n", error, e->klass, e->message);
-		exit(error);
+	exit_on_error(error);
+
+	git_revwalk *walker;
+	error = git_revwalk_new(&walker, repo);
+	exit_on_error(error);
+	error = git_revwalk_push_head(walker);
+	while(!git_revwalk_next(&oid, walker)) {
+		error = git_commit_lookup(&commit, repo, &oid);
+		exit_on_error(error);
+		const char *msg = git_commit_message(commit);
+		printf("%s\n", msg);
 	}
 
-	printf("Hello World\n");
+	return 0;
 }

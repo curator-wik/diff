dd327e9ac0126f5add94b86dcc2ef6cb39ec8207
Juan Gonzalez <supergusano2@gmail.com>
2016-07-26 22:35:55 -0500

    slightly improved code organization

--- main.c
+++ main.c
@@ -15,19 +15,23 @@ void _exit_on_error(int error, const char *func, const int line)
 int main(int argc, char **argv)
 {
 	git_libgit2_init();
+
 	git_oid oid;
 	git_repository *repo = NULL;
 	git_commit *commit = NULL;
-	int error = git_repository_open(&repo, ".");
+	git_revwalk *walker;
+	char oid_str[1024] = {0};
+	int error;
+
+	error = git_repository_open(&repo, ".");
 	exit_on_error(error);
 
-	char oid_str[1024] = {0};
-
-
-	git_revwalk *walker;
 	error = git_revwalk_new(&walker, repo);
 	exit_on_error(error);
+
 	error = git_revwalk_push_head(walker);
+	exit_on_error(error);
+
 	while(!git_revwalk_next(&oid, walker)) {
 		error = git_commit_lookup(&commit, repo, &oid);
 		exit_on_error(error);

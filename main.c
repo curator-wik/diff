17af5eff9caa82e4611b5db0026fdfb94df41140
Juan Gonzalez <supergusano2@gmail.com>
2017-02-19 21:49:43 -0600

    libdmp has an infinite loop. another user has a fork which fixes it, so I've switched to using that for now
    so this example program prints out a git diff and then a raw dmp diff

--- main.c
+++ main.c
@@ -39,10 +39,31 @@ int diff_output(const git_diff_delta *d, const git_diff_hunk *h,
 	    l->origin == GIT_DIFF_LINE_DELETION)
 		c = l->origin;
 
-	//fprintf(fp, "%s: %c%s", oid_str, c, buf);
-	fprintf(fp, "%c%s", c, buf);
+	fprintf(fp, "%s: %c%s", oid_str, c, buf);
+	//fprintf(fp, "%c%s", c, buf);
 	//fwrite(l->content, 1, l->content_len, fp);
 
+	return 0;
+}
+
+struct oid_data {
+	char old[1024];
+	git_oid old_oid;
+	char new[1024];
+	git_oid new_oid;
+};
+
+int git_df_cb(const git_diff_delta *d, float progress, void *payload)
+{
+	struct oid_data *od = (struct oid_data *)payload;
+	char oid_str[1024] = {0};
+	git_oid_tostr((char *)&oid_str, sizeof(oid_str), &d->old_file.id);
+	sprintf(od->old, "%s", oid_str);
+	git_oid_tostr((char *)&oid_str, sizeof(oid_str), &d->new_file.id);
+	sprintf(od->new, "%s", oid_str);
+	//printf("%s\n", oid_str);
+	git_oid_cpy(&od->old_oid, &d->old_file.id);
+	git_oid_cpy(&od->new_oid, &d->new_file.id);
 	return 0;
 }
 
@@ -66,6 +87,29 @@ int main(int argc, char **argv)
 
 	error = git_diff_print(diff, GIT_DIFF_FORMAT_PATCH, diff_output, NULL);
 	exit_on_error(error);
+
+
+	struct oid_data od = {0};
+	error = git_diff_foreach(diff, git_df_cb, NULL, NULL, NULL, &od);
+	exit_on_error(error);
+
+	printf("old: %s\n", od.old);
+	printf("new: %s\n", od.new);
+
+	git_blob *old_blob, *new_blob;
+	error = git_blob_lookup(&old_blob, repo, &od.old_oid);
+	exit_on_error(error);
+	//printf("%s\n", git_blob_rawcontent(old_blob));
+	error = git_blob_lookup(&new_blob, repo, &od.new_oid);
+	if(error) {
+		git_oid work_oid;
+		error = git_blob_create_fromworkdir(&work_oid, repo, "main.c");
+	}
+	error = git_blob_lookup(&new_blob, repo, &od.new_oid);
+	exit_on_error(error);
+
+	char *old_file = strdup(git_blob_rawcontent(old_blob));
+	char *new_file = strdup(git_blob_rawcontent(new_blob));
 #if 0
 	error = git_revwalk_new(&walker, repo);
 	exit_on_error(error);
@@ -83,7 +127,11 @@ int main(int argc, char **argv)
 #endif
 
 	dmp_diff *dmp_df = NULL;
-	dmp_diff_from_strs(&dmp_df, NULL, "This is a test", "This is another test");
+	dmp_options opts;
+	dmp_options_init(&opts);
+	opts.match_threshold = 1;
+	opts.patch_margin = 8;
+	dmp_diff_from_strs(&dmp_df, &opts, old_file, new_file);
 	dmp_diff_print_raw(stdout, dmp_df);
 	dmp_diff_free(dmp_df);
 	return 0;
